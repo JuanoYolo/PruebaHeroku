3fb93f8f8496a126a92556e85e4b33f994757bbb
Juano <juanosierra2@gmail.com>
2022-06-27 09:19:50 -0500

    Creacion de canales

--- src/resources/js/views/Sidebar.js
+++ src/resources/js/views/Sidebar.js
@@ -3,8 +3,43 @@ import React, { useState, useEffect } from "react"
 import { Avatar } from "@material-ui/core";
 import { ExpandMore, Add, Mic, Settings, Headset } from "@material-ui/icons";
 
+import firebaseApp from "../../../firebase/credenciales";
+import { getFirestore, collection, doc, setDoc, getDocs} from "firebase/firestore"
+import { async } from "@firebase/util";
+const db = getFirestore(firebaseApp);
+
 function Sidebar({usuarioGeneral}) {
-    console.log(usuarioGeneral)
+
+    const [ListaCanales, setListaCanales] = useState([]);
+
+async function getCanales(){
+    const canalesArr = [];
+    const collectionRef = collection (db, "canales");
+    const canalesCifrados = await getDocs(collectionRef);
+    canalesCifrados.forEach(canalCifrado=>{
+        canalesArr.push(canalCifrado.data());
+    });
+    setListaCanales(canalesArr);
+}
+
+function agregarCanal(){
+
+    const nombreCanal = prompt("Pon un Nombre al canal");
+
+    if(nombreCanal){
+        const docuRef = doc(db, `canales/${nombreCanal}`);
+        setDoc(docuRef, {
+            id: new Date().getTime(),
+            nombre: nombreCanal,
+        });
+    }
+
+}
+
+useEffect( ()=> {
+    getCanales();
+}, []);
+
     return (
         <div className="sidebar">
             <div className="sidebar__top">
@@ -16,13 +51,15 @@ function Sidebar({usuarioGeneral}) {
                         <h4>Canales de texto</h4>
                     </div>
 
-                    <Add className="sidebar__addChannel" onClick={console.log(
-                        "Canal creado"
-                    )} />
+                    <Add className="sidebar__addChannel" onClick={agregarCanal}
+                     />
                 </div>
 
                 <div className="sidebar__channelsList">
-                    {/*Mapear canales mas adelante*/}
+                    { ListaCanales ? ListaCanales.map((canal)=>{
+                       return <div> {canal.nombre} </div>;
+                    })
+                : null}
 
                 </div>
 
@@ -33,7 +70,7 @@ function Sidebar({usuarioGeneral}) {
                 <Avatar src={usuarioGeneral.photoURL} />
                 <div className="sidebar__profileInfo">
                 <h3>{usuarioGeneral.displayName}</h3>
-                <p>{usuarioGeneral.uid}</p>
+                <p>{usuarioGeneral.uid.substring(0,4)}</p>
                 </div>
             </div>
         </div>
