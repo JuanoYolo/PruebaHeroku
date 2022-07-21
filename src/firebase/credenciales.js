8e711ba190eb0b5e0fd74429a1b646606cda2a4a
JuanoYolo <88836073+JuanoYolo@users.noreply.github.com>
2022-07-21 10:31:20 -0500

    Update credenciales.js

--- src/firebase/credenciales.js
+++ src/firebase/credenciales.js
@@ -3,15 +3,16 @@ import { initializeApp } from "firebase/app";
 
 // Añade aquí tus credenciales
 const firebaseConfig = {
-  apiKey: "AIzaSyDhQLNnNfE3AwM0pgjOp4kxH6MO2leM66M",
-  authDomain: "slackarsw.firebaseapp.com",
-  projectId: "slackarsw",
-  storageBucket: "slackarsw.appspot.com",
-  messagingSenderId: "455848691282",
-  appId: "1:455848691282:web:dc9b97dfd7fc52e59c818b"
+  apiKey: "AIzaSyD4iCAbuDABM-oWpJe4-OtrmkTFfLV3Ec4",
+  authDomain: "slackarsw-ce9c3.firebaseapp.com",
+  projectId: "slackarsw-ce9c3",
+  storageBucket: "slackarsw-ce9c3.appspot.com",
+  messagingSenderId: "397950750502",
+  appId: "1:397950750502:web:f97fd440fb8bc6356e3be7"
 };
 
 // Inicializamos la aplicación y la guardamos en firebaseApp
 const firebaseApp = initializeApp(firebaseConfig);
 // Exportamos firebaseApp para poder utilizarla en cualquier lugar de la aplicación
 export default firebaseApp;
+
